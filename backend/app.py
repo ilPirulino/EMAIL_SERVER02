7fa030c16f6793016ff32c5efe9bfa78f207e909
ilPirulino <alebig2006@gmail.com>
2024-06-06 10:54:44 +0200

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -5,18 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('Return.html')
+    return render_template('index.html')
 
-@app.route('/return', methods=['POST'])
-def save_data():
-    email_sender = request.form['email_sender']
-    email_password = request.form['email_password']
-    email_receiver = request.form['email_receiver']
-    email_subject = request.form['email_subject']
-    email_body = request.form['email_body']
-    email_file = request.form['email_file']
-    bird.Send_Email(email_sender, email_password, email_receiver, email_subject, email_body, email_file)
-    return render_template('Return.html')
+@app.route('/sendemail', methods=['POST'])
+def sendemail():
+    email_sender = 'alebig2006@gmail.com'
+    email_password = 'wlbp kpds qtqr ximv'
+    email_receiver = email_sender
+    email_subject = "Prova invio mail attraverso sito html"
+    link = "https://ilpirulino.github.io/EMAIL_SERVER02/"
+    email_body = f"""<br>Prova avvenuta con successo!<br><br>Link: <a href="{link}">Visita il sito</a>"""
+    bird.Send_Email(email_sender, email_password, email_receiver, email_subject)
+    return "Email sent with success!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
