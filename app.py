eb60f3932b1e955811d22bddd9a5ca5c05695653
ilPirulino <alebig2006@gmail.com>
2024-06-06 11:58:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
-from flask import Flask, render_template, request
+from flask import Flask
 import smtp_code as bird
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('Return.html')
+    return open('index.html').read()
 
-@app.route('/return', methods=['POST'])
-def save_data():
-    email_sender = request.form['email_sender']
-    email_password = request.form['email_password']
-    email_receiver = request.form['email_receiver']
-    email_subject = request.form['email_subject']
-    email_body = request.form['email_body']
-    email_file = request.form['email_file']
-    bird.Send_Email(email_sender, email_password, email_receiver, email_subject, email_body, email_file)
-    return render_template('Return.html')
+@app.route('/sendemail')
+def sendemail():
+    email_sender = 'alebig2006@gmail.com'
+    email_password = 'wlbp kpds qtqr ximv'
+    email_receiver = email_sender
+    email_subject = 'Prova invio email via html'
+    email_body = f"""<br>Prova avvenuta con successo!<br><br>Link: <a href="{link}">Visita il sito</a>"""
+    bird.Send_Email(email_sender, email_password, email_receiver, email_subject, email_body)
+    return "Email sent with sccess!"
 
 if __name__ == '__main__':
     app.run(debug=True)
